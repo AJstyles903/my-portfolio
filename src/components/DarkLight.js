4783d8be33ecae754079cf04051c59fbbe78fddc
AJstyles903 <aryanjay903@gmail.com>
2024-03-20 22:35:08 +0530

    Fix Bug

--- src/components/DarkLight.js
+++ src/components/DarkLight.js
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 const DarkLight = () => {
     const { theme, setTheme } = useTheme()
     return (
-        <div className={`fixed top-4 right-4 ${theme === "light" ? "bg-zinc-900" : "bg-zinc-100"} black rounded-full w-12 h-5`}>
+        <div className={`fixed top-4 right-4 ${theme === "light" ? "bg-zinc-900" : "bg-zinc-100"} black z-[9999] rounded-full w-12 h-5`}>
             <span className={`cursor-pointer absolute top-[-3px] left-[-3px] flex justify-center items-center w-7 h-7 rounded-full bg-zinc-900 transition-all duration-500 ${theme === "dark" ? "translate-x-7" : "translate-x-0"}`}
             >
                 {
